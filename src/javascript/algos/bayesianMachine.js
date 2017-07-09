833591d5c04f31721321906fe4f1b87c4208036a
Osbert Ngok <osbertngok@gmail.com>
2017-07-09 22:13:31 +0800

    real implementation of bayesian machine

--- src/javascript/algos/bayesianMachine.js
+++ src/javascript/algos/bayesianMachine.js
@@ -12,8 +12,16 @@ class BayesianMachine {
   getBanditIndex(round, selectedVector, getResultByBanditIndexFunc) {
     // Calculate Beta Sample for all bandits.
     // If alpha and beta are zero, use uniform distribution
+    /*
     const selectedBanditIndex = utils.argmax(this.scoreboard,
-                                                score => score.positive + score.negative === 0 ? Math.random(): jStat.beta.sample(score.positive, score.negative));
+                                             score => score.positive + score.negative === 0
+                                                      ? Math.random()
+                                                      : jStat.beta.sample(score.positive, score.negative));
+    */
+    const probs = this.scoreboard.map(score => score.positive + score.negative < 100
+             ? Math.random()
+             : jStat.beta.sample(score.positive, score.negative));
+    const selectedBanditIndex = utils.argmax(probs);
     const result = getResultByBanditIndexFunc(selectedBanditIndex);
     this.scoreboard[selectedBanditIndex].process(result);
   }
